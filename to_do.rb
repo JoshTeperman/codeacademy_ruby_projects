e6abc6c3428a12595661a7c9a26de46761ca5fd8
JoshTeperman <36095443+JoshTeperman@users.noreply.github.com>
2018-12-20 22:55:40 +1100

    added IO method, playing with file.load syntax

--- to_do.rb
+++ to_do.rb
@@ -1,50 +1,98 @@
+module Menu
+	
+	def outro 
+		@outro
+	end
+	@outro = "Thanks for using Todo List. See you next time!"
+
+	def menu
+		"Welcome User!
+
+		MENU
+		1)Add
+		2)Show
+		3)Write to a File
+		
+		Type 'q' to Quit the Program \n" 
+	end
+
+	def show
+		menu
+	end
+end
+
+module Promptable
+	def prompt(message="What would you like to do?", symbol='=> ')
+		print message, symbol 
+		gets.chomp
+	end
+end
+
 #classes
 class List
-	attr_reader :name
-	
-	def initialize(name, tasks=[])
-		@name = name
-		@tasks = tasks
+	attr_reader :all_tasks
+
+	def initialize
+	 	@all_tasks = []
+	end
+
+	def add_task(task)
+		all_tasks << task
+		puts "New task '#{task}' added to Todo List."
+		puts task.inspect
 	end
 
 	def show_tasks
-		unless @tasks == [] 
-			puts @tasks
-		else 
-			puts "No tasks set yet" 
-		end
+		all_tasks
+	end	
+
+	def write_to_file(file_name="file_name.txt")
+		new_file = File.new("file_name.txt", "r+") 
+		new_file.puts all_tasks
+		new_file.close
 	end
 
-	def add_task
-		puts "Please write your new task:"
-		task = gets.chomp
-		@tasks.push(task)
-		puts "Task added: #{@tasks}"
-	end
+
 
 end
 
-josh = List.new("Josh")
-tester = List.new("Test_list")
+class Task
+	attr_reader :description
 
-josh.add_task
+  	def initialize(description)
+  		@description = description
+  	end
+
+  	def to_s
+  		description.to_s
+  		#return tasks description as a string
+  	end
+
+end
 
 
+if __FILE__ == $PROGRAM_NAME
+	include Menu
+	include Promptable
 
-
-
-=begin
+	mylist = List.new
+	puts "Welcome to the Todo List. Please choose from the following list:"
+		until ('q').include?(user_input = prompt(show).downcase)
+			case user_input
+				when "1"
+					mylist.add_task(Task.new(prompt("What is the task you would like to add to Todo List?\n")))
+				when "2"
+					puts mylist.show_tasks
+				when "3"
+					mylist.write_to_file
+				else 
+					puts "Get it right, choose '1', '2', or '3'."
+			end
+		end
+	puts Menu.outro
 	
-- List Class
-Create a list
-Add task to list
-Show all tasks
-Read a task from a file
-Write a list to a file
-Delete a task
-Update a task
+end
 
-- Task Class
-Create a task item
-	
-=end	
+	#create a method write_to_file that takes filename as an argument,
+	#and ouputs the alltasks array (descriptions) as a list.
+	#Use an IO class.
